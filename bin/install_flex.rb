4e9b0c059cc826accad8dc7ce85f70df3c175e14
Sean Kelly <sean.kelly@hipponot.com>
2013-03-05 16:53:00 -0700

    script path

--- bin/install_flex.rb
+++ bin/install_flex.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 raise 'Must run as root' unless Process.uid == 0
-Dir.chdir('../kitchen')
+kitchen = File.expand_path(File.join(File.dirname(__FILE__), '..', 'kitchen'))
+Dir.chdir(kitchen)
 cmd = "chef-solo -c solo.rb -j flex.json"
 puts cmd
 puts `#{cmd}`
