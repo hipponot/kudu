3243b5b5f9931989900d272fa54f47655d6434f8
Sean Kelly <sean.kelly@hipponot.com>
2013-10-01 09:24:19 -0600

    default command help

--- lib/kudu/cli.rb
+++ lib/kudu/cli.rb
@@ -18,7 +18,7 @@ module Kudu
 
     check_unknown_options!(:except => [:config, :exec])
 
-    default_task :hello
+    default_task :help
     class_option "no-color", :type => :boolean, :banner => "Disable colorization in output"
     class_option "verbose",  :type => :boolean, :banner => "Enable verbose output mode", :aliases => "-V"
 
