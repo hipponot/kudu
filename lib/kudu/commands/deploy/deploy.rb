629a737eed6621e1768edee661ad64e707fe13f7
Sean Kelly <sean.kelly@simbulus.com>
2013-10-27 18:42:10 -0600

    kudu deploy takes unicorn port

--- lib/kudu/commands/deploy/deploy.rb
+++ lib/kudu/commands/deploy/deploy.rb
@@ -15,24 +15,16 @@ module Kudu
 
     desc "deploy", "Deploy project"
 
-    method_option :name, :aliases => "-n", :required=>false, :default=>nil, :desc => "Project name", :lazy_default => ""
-    method_option :all, :aliases => "-a", :type => :boolean, :required=>false, :desc => "Build everything"
-    method_option :dependencies, :aliases => "-d", :required => false, :desc => "Rebuild dependencies before building"
-    method_option :force, :aliases => "-f", :type => :boolean, :required => false, :default => false,  :desc => "Force rebuild"
-    method_option :repo, :aliases => "-r", :type => :string, :required => false, :default=>"default",  :desc => "Repository name (published artifacts)"
-    
+    method_option :name, :aliases => "-n", :type => :string, :required=>true, :desc => "project name"
+    method_option :port, :aliases => "-p", :type => :string, :required => true, :desc => "port (unicorn)"
+    method_option :repo, :aliases => "-r", :type => :string, :required => false, :default=>"default",  :desc => "repository name"
+
     # No ruby-prof in jruby 
     @profile = RUBY_PLATFORM == 'java' ? false : options[:profile] 
     
     def deploy
       Kudu.with_logging(self, __method__) do
-        if options[:all]
-          DependencyGraph.new.build_order { |project| deploy_one(project) } 
-        elsif options[:dependencies]
-          DependencyGraph.new.build_order(options[:name]).each { |project| deploy_one(project) } 
-        else
-          deploy_one(KuduProject.project(options[:name]))
-        end
+        deploy_one(KuduProject.project(options[:name]))
       end
     end
 
@@ -45,7 +37,6 @@ module Kudu
         Kudu.delegate_command('deploy', project.type, options, project)
       end
     end
-
 
   end
 end
--- lib/kudu/commands/deploy/deploy_sinatra.rb
+++ lib/kudu/commands/deploy/deploy_sinatra.rb
@@ -13,14 +13,11 @@ module Kudu
   class DeploySinatra
 
     def initialize(options, project)
-      raise NotUserVagrant, "deploy command requires user vagrant" if ENV['USER'] != "vagrant"
-      # Hack                                                                                                                                                                                                                                      
-      ports = {"woot_learning_model"=>"3000", "woot_storage"=>"3001", "woot_roster"=>"3002", "woot_cms"=>"3003", "woot_db"=>"3004"}
       `sudo cp #{project.directory}/config/location.conf /etc/nginx/conf.d/location/#{project.name}.conf`
       `sudo cp #{project.directory}/config/upstream.conf /etc/nginx/conf.d/upstream/#{project.name}.conf`
       template = File.join(Kudu.template_dir, "init.d.erb")
       outfile = File.join(project.directory, "build", "#{project.name}.init.d")
-      ErubisInflater.inflate_file_write(template, {project_name:project.name, project_version:project.version, port:ports[project.name]}, outfile)
+      ErubisInflater.inflate_file_write(template, {project_name:project.name, project_version:project.version, port:options[:port]}, outfile)
       `sudo cp #{outfile} /etc/init.d/#{project.name}`
       `sudo chmod 755 /etc/init.d/#{project.name}`
       Kudu.ui.info("#{project.name} deployed")
