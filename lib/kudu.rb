1f9a0404c126740f205143c38ccb06576c39e608
Sean <sean.kelly@wootmath.com>
2021-02-08 10:13:04 -0700

    adjust for change in rvm global gems

--- lib/kudu.rb
+++ lib/kudu.rb
@@ -1,10 +1,32 @@
-require_relative "kudu/bootstrap"
 require_relative "kudu/ui"
+
+def is_installed? gem
+  begin
+    Gem::Specification.find_by_name(gem)
+    true
+  rescue Gem::LoadError
+    false
+  end
+end
+
+def install_require(gem, version)
+  unless is_installed?(gem)
+    system("gem install #{gem} -v #{version} -N")
+    Gem.clear_paths
+  end
+  require gem
+end
+
+require 'fileutils'
+require 'yaml'
+
+install_require 'thor', '1.0.1'
+install_require 'rvm', '1.11.3.9'
+install_require 'erubis', '2.7.0'
 
 module Kudu
 
   class << self
-    Bootstrap.init($*)
     attr_writer :ui
     def ui
       @ui ||= UI.new
--- test/unit/test_rvm.rb
+++ test/unit/test_rvm.rb
@@ -8,21 +8,21 @@ require_relative '../../lib/kudu/rvm_util'
 require_relative '../../lib/kudu/capture_stdout'
 
 class TestRVM < Test::Unit::TestCase
-  
+
   def setup
   end
-  
+
   def teardown
   end
-  
+
 
   def ignore_test_perf
     start = Time.now
-    current = RVM.current 
+    current = RVM.current
     current.gemset.create 'blah'
     current.gemset.use! 'blah'
     10.times {
-      `gem install json -f --no-ri --no-rdoc`
+      `gem install json -f -N`
       `gem uninstall json`
     }
     out1 = `rvm @blah do gem list`
@@ -32,12 +32,12 @@ class TestRVM < Test::Unit::TestCase
     start = Time.now
     `rvm gemset create blah`
     10.times {
-      `rvm @blah do gem install json -f --no-ri --no-rdoc`
+      `rvm @blah do gem install json -f -N`
       `rvm @blah do gem uninstall json`
     }
     out2 = `rvm @blah do gem list`
     `rvm --force gemset delete blah`
-    puts "elapsed #{Time.now - start}"    
+    puts "elapsed #{Time.now - start}"
   end
 
 end
